36dab2b31f8ab7e3043994c07cd5c0fcf037db8a
Fabiana Storer <fabiana.storer@gmail.com>
2025-03-10 23:00:54 +0100

    hotfix

--- src/components/headerNewsSaved/index.jsx
+++ src/components/headerNewsSaved/index.jsx
@@ -8,34 +8,63 @@ const HeaderNewsSaved = () => {
 
   return (
     <header className="header-news-saved">
-      <div className="header-news-saved__wrapper">
-        <h2
-          className={`header-news-saved__title ${
-            showMenu ? "header-news-saved__title_menu" : ""
-          }`}
-        >
-          NewsExplorer
-        </h2>
-        {!showMenu ? (
-          <GiHamburgerMenu
-            color="black"
-            className={`header-news-saved__menu-button`}
-            onClick={() => setShowMenu(true)}
-          />
-        ) : (
-          <IoClose
-            color="white"
-            className={`header-news-saved__menu-button header-news-saved__menu-button_close`}
-            onClick={() => setShowMenu(false)}
-          />
-        )}
-      </div>
-      {showMenu ? (
-        <nav className="header-news-saved__nav">
+      <div className="header__container-title-and-nav">
+        <div className="header-news-saved__wrapper">
+          <h2
+            className={`header-news-saved__title ${
+              showMenu ? "header-news-saved__title_menu" : ""
+            }`}
+          >
+            NewsExplorer
+          </h2>
+          {!showMenu ? (
+            <GiHamburgerMenu
+              color="black"
+              className={`header-news-saved__menu-button`}
+              onClick={() => setShowMenu(true)}
+            />
+          ) : (
+            <IoClose
+              color="white"
+              className={`header-news-saved__menu-button header-news-saved__menu-button_close`}
+              onClick={() => setShowMenu(false)}
+            />
+          )}
+        </div>
+        {showMenu ? (
+          <nav className="header-news-saved__nav">
+            <ul className="header-news-saved__list">
+              <li className="header-news-saved__list-item">
+                <a href="/" className="header-news-saved__link">
+                  Home
+                </a>
+              </li>
+              <li className="header-news-saved__list-item">
+                <a href="/news-saved" className="header-news-saved__link">
+                  Artigos salvos
+                </a>
+              </li>
+              <li className="header-news-saved__list-item">
+                <button className="header-news-saved__signin-button">
+                  Entrar
+                </button>
+              </li>
+            </ul>
+          </nav>
+        ) : null}
+        <nav className="header-news-saved__nav header-news-saved__nav_desktop">
           <ul className="header-news-saved__list">
             <li className="header-news-saved__list-item">
               <a href="/" className="header-news-saved__link">
                 Home
+              </a>
+            </li>
+            <li className="header-news-saved__list-item">
+              <a
+                href="/saved-news"
+                className="header-news-saved__link header-news-saved__link_l"
+              >
+                Artigos salvos
               </a>
             </li>
             <li className="header-news-saved__list-item">
@@ -45,7 +74,7 @@ const HeaderNewsSaved = () => {
             </li>
           </ul>
         </nav>
-      ) : null}
+      </div>
     </header>
   );
 };
